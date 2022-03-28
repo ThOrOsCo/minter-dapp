9b6b37147ee675405e41e5241e8a8b6aa00a2e0d
ThOrOsCo <rivasdayan@gmail.com>
2022-03-28 08:55:06 -0400

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x2C4d7f78c13B641116cDF4464051a74aF1FDfE79"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
